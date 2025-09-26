2450b89add453d376719079e74212aa1f2fb13cf
gracebo2006 <gracebo2006@gmail.com>
2025-09-26 08:59:40 +0200

    please work

--- back_2_gang.gd
+++ back_2_gang.gd
@@ -1,6 +1,1 @@
 extends RigidBody3D
-
-@onready var ani = $animationalso
-
-func _on_blood_trigger_2_body_entered(body: Node) -> void:
-	ani.play("back2ganganim")
--- bloodnew.gd
+++ bloodnew.gd
@@ -3,4 +3,5 @@ extends MeshInstance3D
 @onready var anim = $AnimationPlayer
 
 func _on_blood_trigger_2_body_entered(body: Node) -> void:
-	anim.play("bloodup")
+	if body.name == "Player":
+		anim.play("bloodup")

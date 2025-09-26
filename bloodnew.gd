f7d7ee96a17c1e04e6a6c6173b70ffa62c5e1173
gracebo2006 <gracebo2006@gmail.com>
2025-09-26 09:38:37 +0200

    triggerbacktohallworking

--- bloodnew.gd
+++ bloodnew.gd
@@ -1,7 +1,1 @@
 extends MeshInstance3D
-
-@onready var anim = $AnimationPlayer
-
-func _on_blood_trigger_2_body_entered(body: Node) -> void:
-	if body.name == "Player":
-		anim.play("bloodup")
--- vinrum.gd
+++ vinrum.gd
@@ -1,1 +1,7 @@
 extends Node3D
+
+@onready var anim = $AnimationPlayer
+
+func _on_blood_trigger_2_body_entered(body: Node) -> void:
+	if body.name == "Player":
+		anim.play("bloodup")
